bb4c4004be00934fee6660ed47eec58a10efc125
Miguel Bermudez <miguel.bermudez@controlgroup.com>
2013-10-11 17:38:24 -0400

    start adding img upload via post route

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,44 @@
 var colorthief = require('color-thief')
   , path = require('path')
+  , tmp = require('tmp')
   , fs = require('fs');
 
 epxorts.index = function(req, res) {
-  res.render('index'),
-}
+  res.render('index');
+};
 
 exports.palette = function(req, res) {
+  var tempPath = req.files.file.path;
+  var extname = path.extname(req.files.file.name);
+  var targetPath;
+
+  if (extname.toLowerCase() === '.png' || extname === '.jpg' ) {
+    // create tmp file
+    tmp.file ({
+      mode: 0644,
+      prefix: 'tmpimg-',
+      postfix: extname
+    }, function _tempFileCreated(err, path, fd) {
+      if (err) throw err;
+
+      //set target path for uploaded file
+      targetPath = path.join('./upload', path);
+      fs.rename(tempPath, targetPath, function(err) {
+        if (err) throw err;
+        console.log("Upload Complete!");
+        //TODO: this should end here in a promised,
+        //but for now we'll just get it done
+
+      });
+
+      console.log("tmp file: ", path);
+      console.log("Filedescriptor: ", fd);
+    });
+  } else {
+    fs.unlink(tempPath, function() {
+      res.send(console.log("Only jpgs and pngs are allowed!"));
+    });
+  }
 
 
 }
