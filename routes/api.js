951ac145853718628d71f60ca97e58ca525d54fe
Miguel Bermudez <miguel.bermudez@controlgroup.com>
2013-10-11 17:14:27 -0400

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+var colorthief = require('color-thief')
+  , path = require('path')
+  , fs = require('fs');
+
+epxorts.index = function(req, res) {
+  res.render('index'),
+}
+
+exports.palette = function(req, res) {
+
+
+}
